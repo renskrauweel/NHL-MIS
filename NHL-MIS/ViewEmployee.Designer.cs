485e1fcd6aad34ea8896a25d948559c23ce6fc1c
Richard 1G <jong1737@student.nhl.nl>
2018-01-23 11:28:49 +0100

    IK HEB EMPLOYEBEKIJKENDING GEDAAN

--- NHL-MIS/ViewEmployee.Designer.cs
+++ NHL-MIS/ViewEmployee.Designer.cs
@@ -28,11 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.data7 = new System.Windows.Forms.Label();
+            this.data6 = new System.Windows.Forms.Label();
+            this.data5 = new System.Windows.Forms.Label();
+            this.data4 = new System.Windows.Forms.Label();
+            this.data3 = new System.Windows.Forms.Label();
+            this.data2 = new System.Windows.Forms.Label();
+            this.data1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Medewerker Bekijken";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(814, 600);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 70);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Verwijderen";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1020, 600);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 70);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Aanpassen";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Voornaam";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(70, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Achternaam";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(70, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Functie";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(70, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Opleiding";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(70, 320);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(139, 24);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Datum in dienst";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(70, 355);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 24);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Email";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(70, 390);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(156, 24);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Telefoonnummer";
+            // 
+            // data7
+            // 
+            this.data7.AutoSize = true;
+            this.data7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.data7.Location = new System.Drawing.Point(300, 390);
+            this.data7.Name = "data7";
+            this.data7.Size = new System.Drawing.Size(156, 24);
+            this.data7.TabIndex = 16;
+            this.data7.Text = "Telefoonnummer";
+            // 
+            // data6
+            // 
+            this.data6.AutoSize = true;
+            this.data6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.data6.Location = new System.Drawing.Point(300, 355);
+            this.data6.Name = "data6";
+            this.data6.Size = new System.Drawing.Size(57, 24);
+            this.data6.TabIndex = 15;
+            this.data6.Text = "Email";
+            // 
+            // data5
+            // 
+            this.data5.AutoSize = true;
+            this.data5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.data5.Location = new System.Drawing.Point(300, 320);
+            this.data5.Name = "data5";
+            this.data5.Size = new System.Drawing.Size(139, 24);
+            this.data5.TabIndex = 14;
+            this.data5.Text = "Datum in dienst";
+            // 
+            // data4
+            // 
+            this.data4.AutoSize = true;
+            this.data4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.data4.Location = new System.Drawing.Point(300, 285);
+            this.data4.Name = "data4";
+            this.data4.Size = new System.Drawing.Size(92, 24);
+            this.data4.TabIndex = 13;
+            this.data4.Text = "Opleiding";
+            // 
+            // data3
+            // 
+            this.data3.AutoSize = true;
+            this.data3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.data3.Location = new System.Drawing.Point(300, 250);
+            this.data3.Name = "data3";
+            this.data3.Size = new System.Drawing.Size(73, 24);
+            this.data3.TabIndex = 12;
+            this.data3.Text = "Functie";
+            // 
+            // data2
+            // 
+            this.data2.AutoSize = true;
+            this.data2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.data2.Location = new System.Drawing.Point(300, 215);
+            this.data2.Name = "data2";
+            this.data2.Size = new System.Drawing.Size(112, 24);
+            this.data2.TabIndex = 11;
+            this.data2.Text = "Achternaam";
+            // 
+            // data1
+            // 
+            this.data1.AutoSize = true;
+            this.data1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.data1.Location = new System.Drawing.Point(300, 180);
+            this.data1.Name = "data1";
+            this.data1.Size = new System.Drawing.Size(98, 24);
+            this.data1.TabIndex = 10;
+            this.data1.Text = "Voornaam";
+            // 
+            // ViewEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.data7);
+            this.Controls.Add(this.data6);
+            this.Controls.Add(this.data5);
+            this.Controls.Add(this.data4);
+            this.Controls.Add(this.data3);
+            this.Controls.Add(this.data2);
+            this.Controls.Add(this.data1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ViewEmployee";
             this.Text = "ViewEmployee";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label data7;
+        private System.Windows.Forms.Label data6;
+        private System.Windows.Forms.Label data5;
+        private System.Windows.Forms.Label data4;
+        private System.Windows.Forms.Label data3;
+        private System.Windows.Forms.Label data2;
+        private System.Windows.Forms.Label data1;
     }
 }
