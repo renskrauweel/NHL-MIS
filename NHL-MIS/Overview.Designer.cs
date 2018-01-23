14d011d6f623a7afd75588c0e048b1776500cba0
renskrauweel <renskrauweel@hotmail.com>
2018-01-23 11:52:30 +0100

    Added Utils class + edited overview page

--- NHL-MIS/Overview.Designer.cs
+++ NHL-MIS/Overview.Designer.cs
@@ -28,11 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LABEL";
+            // 
+            // Overview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1275, 715);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Overview";
             this.Text = "Overview";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
---
+++ NHL-MIS/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NHL_MIS
+{
+    class Utils
+    {
+        public void InitialiseForm(Form form)
+        {
+            form.Font = new Font("Arial", 54);
+            form.Width = 1275;
+            form.Height = 715;
+            // titel 28
+        }
+    }
+}
