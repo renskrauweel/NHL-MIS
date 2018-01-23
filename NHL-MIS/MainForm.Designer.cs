d168b2a0749d2ec071e9d481fb003900daf1f17b
”dantheman_0125” <danielroskam@gmail.com>
2018-01-23 10:58:39 +0100

    Version 0.423

--- NHL-MIS/MainForm.Designer.cs
+++ NHL-MIS/MainForm.Designer.cs
@@ -34,9 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(358, 262);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.IsMdiContainer = true;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainForm";
             this.Text = "HRM";
             this.Load += new System.EventHandler(this.MainForm_Load);
